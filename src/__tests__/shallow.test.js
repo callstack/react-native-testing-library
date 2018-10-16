883aea2e4cbc592078c6edef74acb8161acbfe22
Michał Pierzchała <thymikee@gmail.com>
2018-10-16 18:32:37 +0200

    fix flow

--- src/__tests__/shallow.test.js
+++ src/__tests__/shallow.test.js
@@ -3,22 +3,20 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { shallow, render } from '..';
 
-const OnPressComponent = ({ onPress }) => (
+const TextPress = () => (
   <View>
-    <Text onPress={onPress} testID="text-button">
-      Press me
-    </Text>
+    <Text testID="text-button">Press me</Text>
   </View>
 );
 
 test('shallow rendering React elements', () => {
-  const { output } = shallow(<OnPressComponent onPress={jest.fn} />);
+  const { output } = shallow(<TextPress />);
 
   expect(output).toMatchSnapshot();
 });
 
 test('shallow rendering React Test Instance', () => {
-  const { getByTestId } = render(<OnPressComponent onPress={jest.fn} />);
+  const { getByTestId } = render(<TextPress />);
   const { output } = shallow(getByTestId('text-button'));
 
   expect(output).toMatchSnapshot();
