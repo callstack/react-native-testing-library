ff53688343af346d6887f838ac9549785673639b
Michał Pierzchała <thymikee@gmail.com>
2018-11-18 17:20:20 +0100

    update TS

--- typings/__tests__/index.test.tsx
+++ typings/__tests__/index.test.tsx
@@ -63,6 +63,8 @@ const queryAllByTextRegExp: Array<ReactTestInstance> = tree.queryAllByText(
 const queryAllByProps: Array<ReactTestInstance> = tree.getAllByProps({
   value: 2,
 });
+const debugFn = tree.debug()
+const debugFnWithMessage = tree.debug('my message')
 
 tree.update(<View />);
 tree.unmount();
