ad61c409e9f5908a746a87ded92dcd0b7736036a
Brooks Becton <broabect@ut.utm.edu>
2018-12-11 12:58:37 -0600

    docs: add website built with Docusaurus (#74)
    * Run docusaurus init
    
    * Add api.md and remove initial files
    
    * Run docusaurus init
    
    * Add api.md and remove initial files
    
    * Add callstack info and remove extra scripts
    
    * Commeent out  extra features on home page and Add link to example
    
    * Re Arrange Docs Folder
    
    * add some adjustments
    
    * fix lint
    
    * fix lint

---
+++ website/core/Footer.js
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+class Footer extends React.Component {
+  docUrl(doc, language) {
+    const { baseUrl } = this.props.config;
+    return `${baseUrl}docs/${language ? `${language}/` : ''}${doc}`;
+  }
+
+  pageUrl(doc, language) {
+    const { baseUrl } = this.props.config;
+    return baseUrl + (language ? `${language}/` : '') + doc;
+  }
+
+  render() {
+    return (
+      <footer className="nav-footer" id="footer">
+        <section className="sitemap">
+          <a href={this.props.config.baseUrl} className="nav-home">
+            {this.props.config.footerIcon && (
+              <img
+                src={this.props.config.baseUrl + this.props.config.footerIcon}
+                alt={this.props.config.title}
+                width="66"
+                height="58"
+              />
+            )}
+          </a>
+          <div>
+            <h5>Docs</h5>
+            <a
+              href={this.docUrl(
+                'getting-started.html'
+                // TODO: fix i18n
+                /* , this.props.language */
+              )}
+            >
+              Getting Started
+            </a>
+
+            <a
+              href={this.docUrl(
+                'api.html'
+                // TODO: fix i18n
+                /* , this.props.language */
+              )}
+            >
+              API Reference
+            </a>
+          </div>
+          <div>
+            <h5>Community</h5>
+            <a
+              href="http://stackoverflow.com/questions/tagged/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Stack Overflow
+            </a>
+            <a href="https://discord.gg/QbGezWe">Discord channel</a>
+          </div>
+          <div>
+            <h5>More</h5>
+            <a href={this.props.config.repoUrl}>GitHub</a>
+            <a
+              className="github-button"
+              href={this.props.config.repoUrl}
+              data-icon="octicon-star"
+              data-count-href="/callstack/react-native-testing-library/stargazers"
+              data-show-count="true"
+              data-count-aria-label="# stargazers on GitHub"
+              aria-label="Star this project on GitHub"
+            >
+              Star
+            </a>
+          </div>
+        </section>
+
+        {/* <a
+          href="https://code.facebook.com/projects/"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="fbOpenSource"
+        >
+          <img
+            src={`${this.props.config.baseUrl}img/oss_logo.png`}
+            alt="Facebook Open Source"
+            width="170"
+            height="45"
+          />
+        </a> */}
+        <section className="copyright">{this.props.config.copyright}</section>
+      </footer>
+    );
+  }
+}
+
+module.exports = Footer;
---
+++ website/pages/en/index.js
@@ -0,0 +1,140 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react'); // eslint-disable-line import/no-extraneous-dependencies
+
+const {
+  // MarkdownBlock,
+  Container,
+  GridBlock,
+} = require('../../core/CompLibrary.js'); // eslint-disable-line import/no-unresolved
+
+const siteConfig = require(`${process.cwd()}/siteConfig.js`);
+
+// function imgUrl(img) {
+//   return `${siteConfig.baseUrl}img/${img}`;
+// }
+
+function docUrl(doc, language) {
+  return `${siteConfig.baseUrl}docs/${language ? `${language}/` : ''}${doc}`;
+}
+
+// function pageUrl(page, language) {
+//   return siteConfig.baseUrl + (language ? `${language}/` : '') + page;
+// }
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div className="pluginWrapper buttonWrapper">
+        <a className="button" href={this.props.href} target={this.props.target}>
+          {this.props.children}
+        </a>
+      </div>
+    );
+  }
+}
+
+Button.defaultProps = {
+  target: '_self',
+};
+
+const SplashContainer = props => (
+  <div className="homeContainer">
+    <div className="homeSplashFade">
+      <div className="wrapper homeWrapper">{props.children}</div>
+    </div>
+  </div>
+);
+
+// const Logo = props => (
+//   <div className="projectLogo">
+//     <img src={props.img_src} alt="Project Logo" />
+//   </div>
+// );
+
+const ProjectTitle = () => (
+  <React.Fragment>
+    <h2 className="projectTitle">
+      {siteConfig.title}
+      <small>{siteConfig.tagline}</small>
+    </h2>
+    <Button href={docUrl('getting-started')}>Get Started</Button>
+  </React.Fragment>
+);
+
+class HomeSplash extends React.Component {
+  render() {
+    return (
+      <SplashContainer>
+        {/* <Logo img_src={imgUrl('docusaurus.svg')} /> */}
+        <div className="inner">
+          <ProjectTitle />
+        </div>
+      </SplashContainer>
+    );
+  }
+}
+
+const Block = props => (
+  <Container
+    padding={['bottom', 'top']}
+    id={props.id}
+    background={props.background}
+  >
+    <GridBlock align="center" contents={props.children} layout={props.layout} />
+  </Container>
+);
+
+const Features = () => (
+  <Block layout="fourColumn">
+    {[
+      {
+        content:
+          'Built on top of `react-test-renderer`, letting you always be up to date with latest React features',
+        // image: imgUrl('docusaurus.svg'),
+        imageAlign: 'top',
+        title: 'Lightweight',
+      },
+      {
+        content:
+          'Prevents you from testing implementation details because we stand this is a very bad practice',
+        // image: imgUrl('docusaurus.svg'),
+        imageAlign: 'top',
+        title: 'Opinionated',
+      },
+    ]}
+  </Block>
+);
+
+// const FeatureCallout = () => (
+//   <div
+//     className="productShowcaseSection paddingBottom"
+//     style={{ textAlign: 'center' }}
+//   >
+//     <h2>Feature Callout</h2>
+//     <MarkdownBlock>These are features of this project</MarkdownBlock>
+//   </div>
+// );
+
+class Index extends React.Component {
+  render() {
+    const language = this.props.language || '';
+
+    return (
+      <div>
+        <HomeSplash language={language} />
+        <div className="mainContainer">
+          <Features />
+          {/* <FeatureCallout /> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
