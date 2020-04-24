9bfc4a43f20cf5924cd6be761bef6ec9fec543a8
Michał Pierzchała <thymikee@gmail.com>
2020-04-24 09:43:37 +0200

    remove non-essential files

--- examples/reactnavigation/index.js
+++ examples/reactnavigation/index.js
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 
-import { name as appName } from './app.json';
 import App from './src/App';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('ReactNavigationExample', () => App);
