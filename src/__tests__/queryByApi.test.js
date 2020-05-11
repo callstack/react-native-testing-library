458b3d4c1d4dd4270a287d6c8b2a9b0ab96e65b8
Mariia Marchenkova <marchewka@Mariias-MacBook-Pro.local>
2020-05-11 10:28:34 +0200

    remove comments

--- src/__tests__/queryByApi.test.js
+++ src/__tests__/queryByApi.test.js
@@ -3,50 +3,50 @@ import React from 'react';
 import { Text, Image } from 'react-native';
 import { render } from '..';
 
-// test('queryByText nested <Image> in <Text> at start', () => {
-//   expect(
-//     render(
-//       <Text>
-//         <Image source={{}} />
-//         Hello
-//       </Text>
-//     ).queryByText('Hello')
-//   ).toBeTruthy();
-// });
+test('queryByText nested <Image> in <Text> at start', () => {
+  expect(
+    render(
+      <Text>
+        <Image source={{}} />
+        Hello
+      </Text>
+    ).queryByText('Hello')
+  ).toBeTruthy();
+});
 
-// test('queryByText nested <Image> in <Text> at end', () => {
-//   expect(
-//     render(
-//       <Text>
-//         Hello
-//         <Image source={{}} />
-//       </Text>
-//     ).queryByText('Hello')
-//   ).toBeTruthy();
-// });
+test('queryByText nested <Image> in <Text> at end', () => {
+  expect(
+    render(
+      <Text>
+        Hello
+        <Image source={{}} />
+      </Text>
+    ).queryByText('Hello')
+  ).toBeTruthy();
+});
 
-// test('queryByText nested <Image> in <Text> in middle', () => {
-//   expect(
-//     render(
-//       <Text>
-//         Hello
-//         <Image source={{}} />
-//         World
-//       </Text>
-//     ).queryByText('HelloWorld')
-//   ).toBeTruthy();
-// });
+test('queryByText nested <Image> in <Text> in middle', () => {
+  expect(
+    render(
+      <Text>
+        Hello
+        <Image source={{}} />
+        World
+      </Text>
+    ).queryByText('HelloWorld')
+  ).toBeTruthy();
+});
 
-// test('queryByText not found', () => {
-//   expect(
-//     render(
-//       <Text>
-//         Hello
-//         <Image source={{}} />
-//       </Text>
-//     ).queryByText('SomethingElse')
-//   ).toBeFalsy();
-// });
+test('queryByText not found', () => {
+  expect(
+    render(
+      <Text>
+        Hello
+        <Image source={{}} />
+      </Text>
+    ).queryByText('SomethingElse')
+  ).toBeFalsy();
+});
 
 test('queryByText nested multiple <Text> in <Text>', () => {
   expect(
