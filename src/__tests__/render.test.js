1845c9d41406c45bbdfc84f66fab2a3e01f4fa45
Michał Pierzchała <thymikee@gmail.com>
2020-05-14 22:23:13 +0200

    lint fix

--- src/__tests__/render.test.js
+++ src/__tests__/render.test.js
@@ -45,7 +45,7 @@ class Banana extends React.Component<any, any> {
   }
 
   changeFresh = () => {
-    this.setState(state => ({
+    this.setState((state) => ({
       fresh: !state.fresh,
     }));
   };
@@ -320,7 +320,7 @@ test('toJSON', () => {
 });
 
 test('debug', () => {
-  jest.spyOn(console, 'log').mockImplementation(x => x);
+  jest.spyOn(console, 'log').mockImplementation((x) => x);
 
   const { debug } = render(<Banana />);
 
@@ -343,7 +343,7 @@ test('debug', () => {
 });
 
 test('debug changing component', () => {
-  jest.spyOn(console, 'log').mockImplementation(x => x);
+  jest.spyOn(console, 'log').mockImplementation((x) => x);
 
   const { getByProps, debug } = render(<Banana />);
 
