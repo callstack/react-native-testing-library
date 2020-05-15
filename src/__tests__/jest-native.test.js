57390e15a3e9495a10e45093951b38bd69339c3e
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2020-05-15 14:20:38 +0200

    Clarified failing toHaveProps matcher tests

--- src/__tests__/jest-native.test.js
+++ src/__tests__/jest-native.test.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { StyleSheet, View, Button, Text } from 'react-native';
 import { render } from '..';
 import '@testing-library/jest-native/extend-expect';
+import { TestScheduler } from 'jest';
 
 const style = StyleSheet.create({
   style1: {
@@ -75,27 +76,16 @@ test('jest-native matchers work correctly', () => {
   });
 });
 
-test.skip('currently failing jest-native matchers tests', () => {
+test.skip('toHaveProp matchers works correctly', () => {
   const { getByText, getByA11yHint } = render(
     <View>
       <Button title="Enabled Button" onPress={jest.fn()} />
-      <Button title="Disabled Button" disabled={true} onPress={jest.fn()} />
-      <Text accessibilityHint="Empty Text" style={style.style1} />
       <Text accessibilityHint="Not Empty Text">Not empty</Text>
-      <View accessibilityHint="Empty View" />
-      <View accessibilityHint="Not Empty View">
-        <Text />
-      </View>
-      <View accessibilityHint="Container View">
-        <View accessibilityHint="First-Level Child">
-          <Text>Second-Level Child</Text>
-        </View>
-      </View>
     </View>
   );
 
+  // All of the following lines using `toHaveProp` matchers, because it things the looked up prop is `null`.
   expect(getByText('Enabled Button')).toHaveProp('title');
   expect(getByText('Enabled Button')).toHaveProp('title', 'Enabled Button');
-  expect(getByText('Enabled Button')).not.toHaveProp('title', 'Button');
   expect(getByA11yHint('Not Empty Text')).toHaveProp('text', 'Not empty');
 });
