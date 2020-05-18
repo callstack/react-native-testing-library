9b90d6790dbab3a9a5186ddaa49bc78746d562e5
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2020-05-18 13:00:18 +0200

    fix(breaking): `getByTestId` should work only for native elements (#324)
    * Replaced getByTestId with fixed version
    
    * Added tests
    
    * Fixed flow-check
    
    * Updated docs
    
    * Fixed lint error
    
    * Used getByTestId in unit tests instead of queryByTestId
    
    * Updated negative checks in tests

---
+++ src/__tests__/getByApi.test.js
@@ -0,0 +1,37 @@
+// @flow
+import React from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import { render } from '..';
+
+const MyComponent = () => {
+  return <Text>My Component</Text>;
+};
+
+test('getByTestId returns only native elements', () => {
+  const { getByTestId, getAllByTestId } = render(
+    <View>
+      <Text testID="text">Text</Text>
+      <TextInput testID="textInput" />
+      <View testID="view" />
+      <Button testID="button" title="Button" onPress={jest.fn()} />
+      <MyComponent testID="myComponent" />
+    </View>
+  );
+
+  expect(getByTestId('text')).toBeTruthy();
+  expect(getByTestId('textInput')).toBeTruthy();
+  expect(getByTestId('view')).toBeTruthy();
+  expect(getByTestId('button')).toBeTruthy();
+
+  expect(getAllByTestId('text')).toHaveLength(1);
+  expect(getAllByTestId('textInput')).toHaveLength(1);
+  expect(getAllByTestId('view')).toHaveLength(1);
+  expect(getAllByTestId('button')).toHaveLength(1);
+
+  expect(() => getByTestId('myComponent')).toThrowError(
+    'No instances found with testID: myComponent'
+  );
+  expect(() => getAllByTestId('myComponent')).toThrowError(
+    'No instances found with testID: myComponent'
+  );
+});
