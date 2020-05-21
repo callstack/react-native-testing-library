4a84f33a8a98405c7fa67654cb40a7a54c1912aa
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2020-05-21 15:27:08 +0200

    Added async act implementation conditional on React version

--- src/waitFor.js
+++ src/waitFor.js
@@ -1,10 +1,20 @@
 // @flow
 
+import React from 'react';
 import act from './act';
 import { throwRemovedFunctionError } from './helpers/errors';
 
 const DEFAULT_TIMEOUT = 4500;
 const DEFAULT_INTERVAL = 50;
+
+function checkReactVersionAtLeast(major: number, minor: number): boolean {
+  if (React.version === undefined) return false;
+  const [actualMajor, actualMinor] = React.version
+    .split('.')
+    .map((n) => parseInt(n, 10));
+
+  return actualMajor > major || (actualMajor == major && actualMinor >= minor);
+}
 
 export type WaitForOptions = {
   timeout?: number,
@@ -43,9 +53,13 @@ export default async function waitFor<T>(
   expectation: () => T,
   options?: WaitForOptions
 ): Promise<T> {
+  if (!checkReactVersionAtLeast(16, 9)) {
+    return await waitForInternal(expectation, options);
+  }
+
   let result: T;
 
-  //$FlowFixMe: `act` has incorrect typing
+  //$FlowFixMe: `act` has incorrect flow typing
   await act(async () => {
     result = await waitForInternal(expectation, options);
   });
