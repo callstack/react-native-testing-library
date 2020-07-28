3c0a2ecf0210c4c1c197914fe47c0df9e64bd099
Michał Pierzchała <thymikee@gmail.com>
2020-07-28 15:45:27 +0200

    chore(breaking): rename to @testing-library/react-native (#463)

--- examples/reactnavigation/src/__tests__/AppNavigator.test.js
+++ examples/reactnavigation/src/__tests__/AppNavigator.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { render, fireEvent } from 'react-native-testing-library';
+import { render, fireEvent } from '@testing-library/react-native';
 
 import AppNavigator from '../AppNavigator';
 
