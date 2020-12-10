ec0803529953773c145b638668471a277d2e2324
Michał Parkitny <michal.parkitny@callstack.com>
2020-12-10 10:24:44 +0100

    Updated React imports

--- website/src/components/Feature.js
+++ website/src/components/Feature.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import useBaseUrl from '@docusaurus/useBaseUrl';
