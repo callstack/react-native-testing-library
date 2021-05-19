6eb071ac88ff61c755f3171b7ebaaca1ceafe1c4
Damian Sznajder <damsznajder@gmail.com>
2021-05-19 12:37:12 +0200

    refactor: move & remove duplicated tests after merge

--- src/__tests__/getByApi.test.js
+++ src/__tests__/getByApi.test.js
@@ -3,54 +3,6 @@ import React from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 import { render, getDefaultNormalizer } from '..';
 
-const MyComponent = () => {
-  return <Text>My Component</Text>;
-};
-
-test('getByTestId returns only native elements', () => {
-  const { getByTestId, getAllByTestId } = render(
-    <View>
-      <Text testID="text">Text</Text>
-      <TextInput testID="textInput" />
-      <View testID="view" />
-      <Button testID="button" title="Button" onPress={jest.fn()} />
-      <MyComponent testID="myComponent" />
-    </View>
-  );
-
-  expect(getByTestId('text')).toBeTruthy();
-  expect(getByTestId('textInput')).toBeTruthy();
-  expect(getByTestId('view')).toBeTruthy();
-  expect(getByTestId('button')).toBeTruthy();
-
-  expect(getAllByTestId('text')).toHaveLength(1);
-  expect(getAllByTestId('textInput')).toHaveLength(1);
-  expect(getAllByTestId('view')).toHaveLength(1);
-  expect(getAllByTestId('button')).toHaveLength(1);
-
-  expect(() => getByTestId('myComponent')).toThrowError(
-    'Unable to find an element with testID: myComponent'
-  );
-  expect(() => getAllByTestId('myComponent')).toThrowError(
-    'Unable to find an element with testID: myComponent'
-  );
-});
-
-test('supports a regex matcher', () => {
-  const { getByTestId, getAllByTestId } = render(
-    <View>
-      <Text testID="text">Text</Text>
-      <TextInput testID="textInput" />
-      <View testID="view" />
-      <Button testID="button" title="Button" onPress={jest.fn()} />
-      <MyComponent testID="myComponent" />
-    </View>
-  );
-
-  expect(getByTestId(/view/)).toBeTruthy();
-  expect(getAllByTestId(/text/)).toHaveLength(2);
-});
-
 describe('Supports a TextMatch options', () => {
   test('getByText, getAllByText', () => {
     const { getByText, getAllByText } = render(
