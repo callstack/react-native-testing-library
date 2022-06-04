d4f523a2b3a40a2bd6400d834fe5ca3ad35863f9
MattAgn <matthieua@bam.tech>
2022-06-04 17:41:57 +0200

    refactor: reorder imports and object keys

--- src/within.ts
+++ src/within.ts
@@ -3,14 +3,14 @@ import { bindByTextQueries } from './queries/text';
 import { bindByTestIdQueries } from './queries/testId';
 import { bindByDisplayValueQueries } from './queries/displayValue';
 import { bindByPlaceholderTextQueries } from './queries/placeholderText';
+import { bindByLabelTextQueries } from './queries/labelText';
+import { bindByRoleQueries } from './queries/role';
+import { bindByA11yHintQueries } from './queries/a11yHint';
+import { bindByA11yStateQueries } from './queries/a11yState';
+import { bindByA11yStatesQueries } from './queries/a11yStates';
+import { bindByA11yValueQueries } from './queries/a11yValue';
 import { bindUnsafeByTypeQueries } from './queries/unsafeType';
 import { bindUnsafeByPropsQueries } from './queries/unsafeProps';
-import { bindByLabelTextQueries } from './queries/labelText';
-import { bindByA11yHintQueries } from './queries/a11yHint';
-import { bindByRoleQueries } from './queries/role';
-import { bindByA11yStateQueries } from './queries/a11yState';
-import { bindByA11yValueQueries } from './queries/a11yValue';
-import { bindByA11yStatesQueries } from './queries/a11yStates';
 
 export function within(instance: ReactTestInstance) {
   return {
@@ -18,14 +18,14 @@ export function within(instance: ReactTestInstance) {
     ...bindByTestIdQueries(instance),
     ...bindByDisplayValueQueries(instance),
     ...bindByPlaceholderTextQueries(instance),
-    ...bindUnsafeByTypeQueries(instance),
-    ...bindUnsafeByPropsQueries(instance),
     ...bindByLabelTextQueries(instance),
     ...bindByRoleQueries(instance),
     ...bindByA11yHintQueries(instance),
     ...bindByA11yStateQueries(instance),
-    ...bindByA11yValueQueries(instance),
     ...bindByA11yStatesQueries(instance),
+    ...bindByA11yValueQueries(instance),
+    ...bindUnsafeByTypeQueries(instance),
+    ...bindUnsafeByPropsQueries(instance),
   };
 }
 
