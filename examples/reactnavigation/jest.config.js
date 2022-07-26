31d4e2abd1111fe09066ae23f2ccefd1346d6e69
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-07-26 13:31:29 +0200

    chore: update React Navigation deps

--- examples/reactnavigation/jest.config.js
+++ examples/reactnavigation/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'react-native',
   setupFiles: ['./jest-setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation)',
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-native-community|@react-navigation)',
   ],
 };
