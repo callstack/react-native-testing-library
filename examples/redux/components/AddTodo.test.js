b8f564b9b47d5f792a2f45e72e61403e4b9f850e
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-07-26 13:51:04 +0200

    refactor: apply screen, cleanup

--- examples/redux/components/AddTodo.test.js
+++ examples/redux/components/AddTodo.test.js
@@ -1,40 +1,36 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import { fireEvent, render } from '@testing-library/react-native';
+import { render, screen, fireEvent } from '@testing-library/react-native';
 import configureStore from '../store';
 import AddTodo from './AddTodo';
 
-describe('Application test', () => {
-  test('adds a new test when entry has been included', () => {
-    const store = configureStore();
+test('adds a new test when entry has been included', () => {
+  const store = configureStore();
 
-    const component = (
-      <Provider store={store}>
-        <AddTodo />
-      </Provider>
-    );
+  const component = (
+    <Provider store={store}>
+      <AddTodo />
+    </Provider>
+  );
 
-    const { getByPlaceholderText, getByText } = render(component);
+  render(component);
 
-    const input = getByPlaceholderText(/repository/i);
-    expect(input).toBeTruthy();
+  const input = screen.getByPlaceholderText(/repository/i);
+  expect(input).toBeTruthy();
 
-    const textToEnter = 'This is a random element';
-    fireEvent.changeText(input, textToEnter);
-    fireEvent.press(getByText('Submit form'));
+  const textToEnter = 'This is a random element';
+  fireEvent.changeText(input, textToEnter);
+  fireEvent.press(screen.getByText('Submit form'));
 
-    const todosState = store.getState().todos;
-
-    expect(todosState.length).toEqual(1);
-
-    expect(todosState).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          id: 1,
-          text: textToEnter,
-          date: expect.any(Date),
-        }),
-      ])
-    );
-  });
+  const todosState = store.getState().todos;
+  expect(todosState.length).toEqual(1);
+  expect(todosState).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        id: 1,
+        text: textToEnter,
+        date: expect.any(Date),
+      }),
+    ])
+  );
 });
--- examples/redux/components/TodoList.test.js
+++ examples/redux/components/TodoList.test.js
@@ -1,57 +1,45 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import { fireEvent, render } from '@testing-library/react-native';
+import { render, screen, fireEvent } from '@testing-library/react-native';
 import configureStore from '../store';
 import TodoList from './TodoList';
 
-describe('Application test', () => {
-  test('it should execute with a store with 4 elements', () => {
-    const initialState = {
-      todos: [
-        { id: 1, text: 'Sing something', date: new Date() },
-        { id: 2, text: 'Dance something', date: new Date() },
-        { id: 3, text: 'Sleep something', date: new Date() },
-        { id: 4, text: 'Sleep something', date: new Date() },
-      ],
-    };
-    const store = configureStore(initialState);
+const initialState = {
+  todos: [
+    { id: 1, text: 'Sing something', date: new Date() },
+    { id: 2, text: 'Dance something', date: new Date() },
+    { id: 3, text: 'Sleep something', date: new Date() },
+    { id: 4, text: 'Sleep something', date: new Date() },
+  ],
+};
 
-    const component = (
-      <Provider store={store}>
-        <TodoList />
-      </Provider>
-    );
+test('it should execute with a store with 4 elements', () => {
+  const store = configureStore(initialState);
+  render(
+    <Provider store={store}>
+      <TodoList />
+    </Provider>
+  );
 
-    const { getAllByText } = render(component);
-    const todoElems = getAllByText(/something/i);
+  const todoElems = screen.getAllByText(/something/i);
+  expect(todoElems.length).toEqual(4);
+});
 
-    expect(todoElems.length).toEqual(4);
-  });
+test('should execute with 2 elements and end up with 1 after delete', () => {
+  const store = configureStore(initialState);
+  render(
+    <Provider store={store}>
+      <TodoList />
+    </Provider>
+  );
 
-  test('should execute with 2 elements and end up with 1 after delete', () => {
-    const initialState = {
-      todos: [
-        { id: 1, text: 'Sing something', date: new Date() },
-        { id: 2, text: 'Dance something', date: new Date() },
-      ],
-    };
-    const store = configureStore(initialState);
+  const todoElems = screen.getAllByText(/something/i);
+  expect(todoElems.length).toBe(4);
 
-    const component = (
-      <Provider store={store}>
-        <TodoList />
-      </Provider>
-    );
+  const buttons = screen.getAllByText('Delete');
+  expect(buttons.length).toBe(4);
+  fireEvent.press(buttons[0]);
 
-    const { getAllByText } = render(component);
-    const todoElems = getAllByText(/something/i);
-
-    expect(todoElems.length).toBe(2);
-
-    const buttons = getAllByText('Delete');
-    expect(buttons.length).toBe(2);
-
-    fireEvent.press(buttons[0]);
-    expect(getAllByText('Delete').length).toBe(1);
-  });
+  const buttonsAfterDelete = screen.getAllByText('Delete');
+  expect(buttonsAfterDelete.length).toBe(3);
 });
