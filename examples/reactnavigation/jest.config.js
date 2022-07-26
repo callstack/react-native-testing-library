f6fa6551ca5f5e9a8bc6d99dacd4d2e2c7cf6b04
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-07-26 14:25:34 +0200

    chore: remove commented setting

--- examples/reactnavigation/jest.config.js
+++ examples/reactnavigation/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'react-native',
-  // setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   setupFilesAfterEnv: ['./jest-setup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|@react-native-community|@react-navigation)',
