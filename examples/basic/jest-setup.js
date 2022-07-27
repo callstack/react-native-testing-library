8e454340aba3ca426e13c476b91b9907acc31f8e
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-07-27 13:22:14 +0200

    refactor: fix lint issues

--- examples/basic/jest-setup.js
+++ examples/basic/jest-setup.js
@@ -1,2 +1,4 @@
+/* eslint-disable no-undef */
+
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
