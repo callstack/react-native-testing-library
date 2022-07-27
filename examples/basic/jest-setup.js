7c736ce3a344ebd57f7e1011f40a355e81f8ca4f
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-07-27 10:58:32 +0200

    chore: extract jest.config.js and add jest-setup.js

---
+++ examples/basic/jest-setup.js
@@ -0,0 +1,2 @@
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
