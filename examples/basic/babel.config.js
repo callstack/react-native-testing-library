1a3fcc5f7d3b173a7d288cf1de16528237d8ef0f
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-07-28 09:22:16 +0200

    refactor: migrat to expo

--- examples/basic/babel.config.js
+++ examples/basic/babel.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
