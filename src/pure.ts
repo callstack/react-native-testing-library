cf3c8d03b0b260f45a9ee2df7f83d2a6751eee19
Evan Walsh <evan@hellowelcome.org>
2022-08-09 09:59:18 -0400

    Add result and option types to `pure` exports

--- src/pure.ts
+++ src/pure.ts
@@ -1,15 +1,15 @@
 import act from './act';
 import cleanup from './cleanup';
 import fireEvent from './fireEvent';
-import render, { RenderResult } from './render';
+import render, { RenderResult, RenderOptions } from './render';
 import waitFor from './waitFor';
 import waitForElementToBeRemoved from './waitForElementToBeRemoved';
 import { within, getQueriesForElement } from './within';
 import { getDefaultNormalizer } from './matches';
-import { renderHook } from './renderHook';
+import { renderHook, RenderHookOptions, RenderHookResult } from './renderHook';
 import { screen } from './screen';
 
-export type { RenderResult };
+export type { RenderOptions, RenderResult };
 export type RenderAPI = RenderResult;
 
 export { act };
@@ -20,5 +20,5 @@ export { waitFor };
 export { waitForElementToBeRemoved };
 export { within, getQueriesForElement };
 export { getDefaultNormalizer };
-export { renderHook };
+export { renderHook, RenderHookOptions, RenderHookResult };
 export { screen };
