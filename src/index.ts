729626a8f23a1dfd79ee77ef13a22f9d2dfac348
Augustin Le Fèvre <augustin.le-fevre@klarna.com>
2022-09-22 14:28:48 +0200

    Improve readability

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,32 @@ import { cleanup } from './pure';
 import { flushMicroTasks } from './flushMicroTasks';
 import { getIsReactActEnvironment, setReactActEnvironment } from './act';
 
-// If we're running in a test runner that supports afterEach
-// then we'll automatically run cleanup afterEach test
-// this ensures that tests run in isolation from each other
-// if you don't like this then either import the `pure` module
-// or set the RNTL_SKIP_AUTO_CLEANUP env variable to 'true'.
-if (typeof afterEach === 'function' && !process.env.RNTL_SKIP_AUTO_CLEANUP) {
-  // eslint-disable-next-line no-undef
-  afterEach(async () => {
-    await flushMicroTasks();
-    cleanup();
-  });
-}
+if (typeof process === 'undefined' || !process.env?.RNTL_SKIP_AUTO_CLEANUP) {
+  // If we're running in a test runner that supports afterEach
+  // then we'll automatically run cleanup afterEach test
+  // this ensures that tests run in isolation from each other
+  // if you don't like this then either import the `pure` module
+  // or set the RNTL_SKIP_AUTO_CLEANUP env variable to 'true'.
+  if (typeof afterEach === 'function') {
+    // eslint-disable-next-line no-undef
+    afterEach(async () => {
+      await flushMicroTasks();
+      cleanup();
+    });
+  }
 
-if (
-  typeof beforeAll === 'function' &&
-  typeof afterAll === 'function' &&
-  !process.env.RNTL_SKIP_AUTO_CLEANUP
-) {
-  // This matches the behavior of React < 18.
-  let previousIsReactActEnvironment = getIsReactActEnvironment();
-  beforeAll(() => {
-    previousIsReactActEnvironment = getIsReactActEnvironment();
-    setReactActEnvironment(true);
-  });
+  if (typeof beforeAll === 'function' && typeof afterAll === 'function') {
+    // This matches the behavior of React < 18.
+    let previousIsReactActEnvironment = getIsReactActEnvironment();
+    beforeAll(() => {
+      previousIsReactActEnvironment = getIsReactActEnvironment();
+      setReactActEnvironment(true);
+    });
 
-  afterAll(() => {
-    setReactActEnvironment(previousIsReactActEnvironment);
-  });
+    afterAll(() => {
+      setReactActEnvironment(previousIsReactActEnvironment);
+    });
+  }
 }
 
 export * from './pure';
