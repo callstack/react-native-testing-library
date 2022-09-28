db00f57a11733d18a3d515ec00b67cda25e5eb3e
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-09-28 11:41:51 +0200

    chore: deeply nested text

--- src/__tests__/tripwire.test.tsx
+++ src/__tests__/tripwire.test.tsx
@@ -30,6 +30,47 @@ test('React Native tripwire: <Text> renders single host element', () => {
   `);
 });
 
+test('React Native tripwire: nested <Text> renders single host element', () => {
+  const view = render(
+    <Text testID="test">
+      <Text testID="before">Before</Text>
+      Hello
+      <Text testID="after">
+        <Text testID="deeplyNested">Deeply nested</Text>
+      </Text>
+    </Text>
+  );
+  expect(getHostSelf(view.getByText('Hello'))).toBe(view.getByTestId('test'));
+  expect(getHostSelf(view.getByText('Before'))).toBe(
+    view.getByTestId('before')
+  );
+  expect(getHostSelf(view.getByText('Deeply nested'))).toBe(
+    view.getByTestId('deeplyNested')
+  );
+
+  expect(view.toJSON()).toMatchInlineSnapshot(`
+    <Text
+      testID="test"
+    >
+      <Text
+        testID="before"
+      >
+        Before
+      </Text>
+      Hello
+      <Text
+        testID="after"
+      >
+        <Text
+          testID="deeplyNested"
+        >
+          Deeply nested
+        </Text>
+      </Text>
+    </Text>
+  `);
+});
+
 test('React Native tripwire: <TextInput> renders single host element', () => {
   const view = render(
     <TextInput
