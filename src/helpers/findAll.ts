13b0d730ba25fbd49be13566c2aed10461611481
EBAM006 <matthieu.alingrin.bam@ext.pmu.fr>
2022-10-08 09:02:34 +0200

    feat: implement new findAll helper including hidden option

---
+++ src/helpers/findAll.ts
@@ -0,0 +1,39 @@
+import { ReactTestInstance } from 'react-test-renderer';
+import { AccessibilityOption } from '../queries/accessibilityOption';
+import { isInaccessible, isSubtreeInaccessible } from './accessiblity';
+
+export function findAll<Options extends AccessibilityOption>(
+  instance: ReactTestInstance,
+  predicate: (node: ReactTestInstance) => boolean,
+  options?: Options
+) {
+  const results = instance.findAll(predicate);
+
+  //TODO: implement getConfig
+  const defaultHidden = true;
+  const hidden = options?.hidden ?? defaultHidden; // We will want to add `defaultHidden: boolean` option to `configure`
+
+  if (hidden) {
+    return results;
+  }
+
+  const subtreeIsInaccessibleCache = new WeakMap<ReactTestInstance>();
+  function cachedIsSubtreeInaccessible(element: ReactTestInstance | null) {
+    if (element === null) {
+      return true;
+    }
+
+    if (!subtreeIsInaccessibleCache.has(element)) {
+      subtreeIsInaccessibleCache.set(element, isSubtreeInaccessible(element));
+    }
+
+    return subtreeIsInaccessibleCache.get(element);
+  }
+
+  return results.filter(
+    (result) =>
+      !isInaccessible(result, {
+        isSubtreeInaccessible: cachedIsSubtreeInaccessible,
+      })
+  );
+}
