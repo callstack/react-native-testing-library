39be70aaffab6fad2b2da6ffb2e15b3d855c7666
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-11-23 14:45:46 +0100

    feature: add drawer navigation

--- examples/react-navigation/jest-setup.js
+++ examples/react-navigation/jest-setup.js
@@ -1,7 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure } from '@testing-library/react-native';
 
+// Import Jest Native matchers
+import '@testing-library/jest-native/extend-expect';
+
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
-configure({ defaultIncludeHiddenElements: false });
+// Setup Reanimated mocking for Drawer navigation
+global.ReanimatedDataMock = { now: () => Date.now() };
+require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
+
+// Enable excluding hidden elements from the queries by default
+configure({
+  defaultIncludeHiddenElements: false,
+});
---
+++ examples/react-navigation/src/DrawerNavigator.test.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { screen, fireEvent } from '@testing-library/react-native';
+import { renderWithNavigation } from './test-utils';
+import DrawerNavigator from './DrawerNavigator';
+
+test('Changing screens', () => {
+  renderWithNavigation(<DrawerNavigator />);
+
+  // Assert initial screen
+  expect(screen.getByRole('header', { name: 'Home screen' })).toBeTruthy();
+
+  // Open drawer by pressing button
+  const toggleButton = screen.getByText('Toggle drawer');
+  fireEvent.press(toggleButton);
+
+  // Assert drawer state
+  expect(screen.getByRole('button', { name: 'Home' })).toHaveAccessibilityState(
+    { selected: true }
+  );
+  expect(
+    screen.getByRole('button', { name: 'Settings' })
+  ).toHaveAccessibilityState({ selected: false });
+
+  // Press drawer item
+  fireEvent.press(screen.getByRole('button', { name: 'Settings' }));
+
+  // Assert drawer state after action
+  expect(screen.getByRole('button', { name: 'Home' })).toHaveAccessibilityState(
+    { selected: false }
+  );
+  expect(
+    screen.getByRole('button', { name: 'Settings' })
+  ).toHaveAccessibilityState({ selected: true });
+
+  // Assert visible screen
+  expect(screen.getByRole('header', { name: 'Settings screen' })).toBeTruthy();
+  expect(screen.queryByRole('header', { name: 'Home screen' })).toBeFalsy();
+});
