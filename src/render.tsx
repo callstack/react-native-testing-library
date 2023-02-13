57e333a0e20fa70c8b4996616cbab3eed4c4b18a
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-02-14 00:56:53 +0100

    fix: ci issues

--- src/render.tsx
+++ src/render.tsx
@@ -111,7 +111,7 @@ function buildRenderResult(
       return getHostChildren(instance)[0];
     },
     UNSAFE_root: instance,
-    get container() {
+    get container(): ReactTestInstance {
       throw new Error(
         "'container' property has been renamed to 'UNSAFE_root'.\n\n" +
           "Consider using 'root' property which returns root host element."
