a2280bf33b3996ed5b95fcb0ead0262060116dd3
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-03-06 11:42:47 +0100

    chore: remove dep @babel/plugin-proposal-class-properties

--- babel.config.js
+++ babel.config.js
@@ -12,11 +12,4 @@ module.exports = {
       },
     ],
   ],
-  plugins: ['@babel/plugin-proposal-class-properties'],
-  env: {
-    test: {
-      // https://github.com/react-native-community/upgrade-support/issues/152
-      plugins: ['@babel/plugin-transform-flow-strip-types'],
-    },
-  },
 };
