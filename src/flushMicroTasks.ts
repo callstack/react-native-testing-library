9410b51534f169a9474ac49d37f0347cc4294dbb
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-03-23 11:13:41 +0100

    refactor: flush microtask queue implementation

--- src/flushMicroTasks.ts
+++ src/flushMicroTasks.ts
@@ -1,13 +1,5 @@
 import { setImmediate } from './helpers/timers';
 
-type Thenable<T> = { then: (callback: () => T) => unknown };
-
-export function flushMicroTasks(): Thenable<void> {
-  return {
-    // using "thenable" instead of a Promise, because otherwise it breaks when
-    // using "modern" fake timers
-    then(resolve) {
-      setImmediate(resolve);
-    },
-  };
+export function flushMicroTasks() {
+  return new Promise((resolve) => setImmediate(resolve));
 }
