f8e4d3e0fb2a706f5ef431a31b60e3bdbddb38e1
pierrezimmermann <pierrez@nam.tech>
2023-04-13 12:27:41 +0200

    refactor: move userEvent.press tests in a dedicated test file

--- src/__tests__/userEvent/userEvent.press.test.tsx
+++ src/__tests__/userEvent/userEvent.press.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
-import { render, screen, userEvent } from '../pure';
+import { render, screen, userEvent } from '../../pure';
 
 jest.useFakeTimers();
 
