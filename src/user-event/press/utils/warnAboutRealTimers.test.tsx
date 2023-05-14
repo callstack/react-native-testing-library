cddd4f7456db125e70d1a036b39b3a56b7f5c047
pierrezimmermann <pierrez@nam.tech>
2023-05-14 23:09:23 +0200

    chore: improve coverage

---
+++ src/user-event/press/utils/warnAboutRealTimers.test.tsx
@@ -0,0 +1,13 @@
+import { warnAboutRealTimers } from './warnAboutRealTimers';
+
+const mockConsoleWarn = jest.spyOn(console, 'warn').mockImplementation();
+
+test('logs a warning about usign real timers with user event', () => {
+  warnAboutRealTimers();
+
+  expect(mockConsoleWarn.mock.calls[0][0]).toMatchInlineSnapshot(`
+    "It is not recommended to use userEvent without using fake timers
+    Some events involve duration so your tests may take a long time to run.
+    For instance calling userEvent.longPress with real timers will take 500ms"
+  `);
+});
