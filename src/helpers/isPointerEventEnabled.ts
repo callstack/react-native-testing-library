b6b0d30023891d6088055b4b96afdf773427d6e9
pierrezimmermann <pierrez@nam.tech>
2023-05-31 09:52:41 +0200

    refactor: use ts doc for isPointerEventsEnabled method

--- src/helpers/isPointerEventEnabled.ts
+++ src/helpers/isPointerEventEnabled.ts
@@ -1,12 +1,13 @@
 import { ReactTestInstance } from 'react-test-renderer';
 import { getHostParent } from './component-tree';
 
-// pointerEvents controls whether the View can be the target of touch events.
-// 'auto': The View can be the target of touch events.
-// 'none': The View is never the target of touch events.
-// 'box-none': The View is never the target of touch events but its subviews can be
-// 'box-only': The view can be the target of touch events but its subviews cannot be
-// see the official react native doc https://reactnative.dev/docs/view#pointerevents
+/**
+ * pointerEvents controls whether the View can be the target of touch events.
+ * 'auto': The View and its children can be the target of touch events.
+ * 'none': The View is never the target of touch events.
+ * 'box-none': The View is never the target of touch events but its subviews can be
+ * 'box-only': The view can be the target of touch events but its subviews cannot be
+ * see the official react native doc https://reactnative.dev/docs/view#pointerevents */
 export const isPointerEventEnabled = (
   element: ReactTestInstance,
   isParent?: boolean
