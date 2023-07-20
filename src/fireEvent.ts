7587e6981b6397a932456d9b06accc0674cdbce3
Pierre Zimmermann <64224599+pierrezimmermannbam@users.noreply.github.com>
2023-07-20 17:04:06 +0200

    feat: add autocomplete for fireEvent event names (#1434)
    * feat: add autocomplete for fireEvent event names
    
    * feat: uncapitalize fireEvent autocomplete propositions
    
    * feat: add textinput props to the autocomplete
    
    * refactor: fix textinput props reference
    
    ---------
    
    Co-authored-by: pierrezimmermann <pierrez@nam.tech>
    Co-authored-by: Maciej Jastrzębski <mdjastrzebski@gmail.com>

--- src/fireEvent.ts
+++ src/fireEvent.ts
@@ -1,4 +1,11 @@
 import { ReactTestInstance } from 'react-test-renderer';
+import {
+  ViewProps,
+  TextProps,
+  TextInputProps,
+  PressableProps,
+  ScrollViewProps,
+} from 'react-native';
 import act from './act';
 import { isHostElement } from './helpers/component-tree';
 import { getHostComponentNames } from './helpers/host-component-names';
@@ -108,9 +115,27 @@ function getEventHandlerName(eventName: string) {
   return `on${eventName.charAt(0).toUpperCase()}${eventName.slice(1)}`;
 }
 
+// Allows any string but will provide autocomplete for type T
+type StringWithAutoComplete<T> = T | (string & Record<never, never>);
+
+// String union type of keys of T that start with on, stripped from on
+type OnKeys<T> = keyof {
+  [K in keyof T as K extends `on${infer Rest}`
+    ? Uncapitalize<Rest>
+    : never]: T[K];
+};
+
+type EventName = StringWithAutoComplete<
+  | OnKeys<ViewProps>
+  | OnKeys<TextProps>
+  | OnKeys<TextInputProps>
+  | OnKeys<PressableProps>
+  | OnKeys<ScrollViewProps>
+>;
+
 function fireEvent(
   element: ReactTestInstance,
-  eventName: string,
+  eventName: EventName,
   ...data: unknown[]
 ) {
   const handler = findEventHandler(element, eventName);
