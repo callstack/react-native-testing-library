109dc4ceb7add03f791e28c2567e765b34cf9a30
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2023-08-08 13:21:28 +0200

    refactor: cleanup

--- src/user-event/utils/host-components.ts
+++ src/user-event/utils/host-components.ts
@@ -1,11 +1,6 @@
 import { ReactTestInstance } from 'react-test-renderer';
-import { isPointerEventEnabled } from '../../helpers/pointer-events';
 import { isHostTextInput } from '../../helpers/host-component-names';
 
 export function isEditableTextInput(element: ReactTestInstance) {
   return isHostTextInput(element) && element.props.editable !== false;
-}
-
-export function isFocusableTextInput(element: ReactTestInstance) {
-  return isHostTextInput(element) && isPointerEventEnabled(element);
 }
