62c6ea8bf424689b165c238bed18ee380e8f6ee6
Wiktor <wiktor.sieprawski@rumblefish.pl>
2023-08-08 21:05:04 +0200

    feat: momentum scroll

--- src/user-event/scroll/__tests__/scroll.test.tsx
+++ src/user-event/scroll/__tests__/scroll.test.tsx
@@ -180,4 +180,263 @@ describe('userEvent.scroll with fake timers', () => {
       ]
     `);
   });
+
+  test('calls events with momentum', async () => {
+    const { events, logEvent } = createEventLogger();
+    const user = userEvent.setup();
+
+    render(
+      <ScrollView
+        onScroll={logEvent('scroll')}
+        onScrollBeginDrag={logEvent('scrollBeginDrag')}
+        onScrollEndDrag={logEvent('scrollEndDrag')}
+        onMomentumScrollBegin={logEvent('momentumScrollBegin')}
+        onMomentumScrollEnd={logEvent('momentumScrollEnd')}
+        onScrollToTop={logEvent('scrollToTop')}
+        testID="scrollable"
+      />
+    );
+
+    await user.scroll(screen.getByTestId('scrollable'), {
+      offset: { x: 0, y: 120 },
+      momentumScroll: { value: 30, steps: 1 },
+    });
+
+    expect(events).toMatchInlineSnapshot(`
+      [
+        {
+          "name": "scrollBeginDrag",
+          "payload": {
+            "contentInset": {
+              "bottom": 0,
+              "left": 0,
+              "right": 0,
+              "top": 0,
+            },
+            "contentOffset": {
+              "x": 0,
+              "y": 0,
+            },
+            "contentSize": {
+              "height": 0,
+              "width": 0,
+            },
+            "layoutMeasurement": {
+              "height": 0,
+              "width": 0,
+            },
+            "responderIgnoreScroll": true,
+            "target": 0,
+            "velocity": {
+              "x": 0,
+              "y": 0,
+            },
+          },
+        },
+        {
+          "name": "scroll",
+          "payload": {
+            "contentInset": {
+              "bottom": 0,
+              "left": 0,
+              "right": 0,
+              "top": 0,
+            },
+            "contentOffset": {
+              "x": 0,
+              "y": 30,
+            },
+            "contentSize": {
+              "height": 0,
+              "width": 0,
+            },
+            "layoutMeasurement": {
+              "height": 0,
+              "width": 0,
+            },
+            "responderIgnoreScroll": true,
+            "target": 0,
+            "velocity": {
+              "x": 0,
+              "y": 0,
+            },
+          },
+        },
+        {
+          "name": "scroll",
+          "payload": {
+            "contentInset": {
+              "bottom": 0,
+              "left": 0,
+              "right": 0,
+              "top": 0,
+            },
+            "contentOffset": {
+              "x": 0,
+              "y": 60,
+            },
+            "contentSize": {
+              "height": 0,
+              "width": 0,
+            },
+            "layoutMeasurement": {
+              "height": 0,
+              "width": 0,
+            },
+            "responderIgnoreScroll": true,
+            "target": 0,
+            "velocity": {
+              "x": 0,
+              "y": 0,
+            },
+          },
+        },
+        {
+          "name": "scroll",
+          "payload": {
+            "contentInset": {
+              "bottom": 0,
+              "left": 0,
+              "right": 0,
+              "top": 0,
+            },
+            "contentOffset": {
+              "x": 0,
+              "y": 90,
+            },
+            "contentSize": {
+              "height": 0,
+              "width": 0,
+            },
+            "layoutMeasurement": {
+              "height": 0,
+              "width": 0,
+            },
+            "responderIgnoreScroll": true,
+            "target": 0,
+            "velocity": {
+              "x": 0,
+              "y": 0,
+            },
+          },
+        },
+        {
+          "name": "scrollEndDrag",
+          "payload": {
+            "contentInset": {
+              "bottom": 0,
+              "left": 0,
+              "right": 0,
+              "top": 0,
+            },
+            "contentOffset": {
+              "x": 0,
+              "y": 120,
+            },
+            "contentSize": {
+              "height": 0,
+              "width": 0,
+            },
+            "layoutMeasurement": {
+              "height": 0,
+              "width": 0,
+            },
+            "responderIgnoreScroll": true,
+            "target": 0,
+            "velocity": {
+              "x": 0,
+              "y": 0,
+            },
+          },
+        },
+        {
+          "name": "momentumScrollBegin",
+          "payload": {
+            "contentInset": {
+              "bottom": 0,
+              "left": 0,
+              "right": 0,
+              "top": 0,
+            },
+            "contentOffset": {
+              "x": 0,
+              "y": 120,
+            },
+            "contentSize": {
+              "height": 0,
+              "width": 0,
+            },
+            "layoutMeasurement": {
+              "height": 0,
+              "width": 0,
+            },
+            "responderIgnoreScroll": true,
+            "target": 0,
+            "velocity": {
+              "x": 0,
+              "y": 0,
+            },
+          },
+        },
+        {
+          "name": "scroll",
+          "payload": {
+            "contentInset": {
+              "bottom": 0,
+              "left": 0,
+              "right": 0,
+              "top": 0,
+            },
+            "contentOffset": {
+              "x": 0,
+              "y": 135,
+            },
+            "contentSize": {
+              "height": 0,
+              "width": 0,
+            },
+            "layoutMeasurement": {
+              "height": 0,
+              "width": 0,
+            },
+            "responderIgnoreScroll": true,
+            "target": 0,
+            "velocity": {
+              "x": 0,
+              "y": 0,
+            },
+          },
+        },
+        {
+          "name": "momentumScrollEnd",
+          "payload": {
+            "contentInset": {
+              "bottom": 0,
+              "left": 0,
+              "right": 0,
+              "top": 0,
+            },
+            "contentOffset": {
+              "x": 0,
+              "y": 150,
+            },
+            "contentSize": {
+              "height": 0,
+              "width": 0,
+            },
+            "layoutMeasurement": {
+              "height": 0,
+              "width": 0,
+            },
+            "responderIgnoreScroll": true,
+            "target": 0,
+            "velocity": {
+              "x": 0,
+              "y": 0,
+            },
+          },
+        },
+      ]
+    `);
+  });
 });
--- src/user-event/scroll/scroll.ts
+++ src/user-event/scroll/scroll.ts
@@ -7,6 +7,12 @@ import { ContentOffset } from '../event-builder/scroll';
 export interface ScrollOptions {
   offset: ContentOffset;
   steps?: number;
+  momentumScroll?: MomentumScroll;
+}
+
+export interface MomentumScroll {
+  value: number;
+  steps?: number;
 }
 
 export async function scroll(
@@ -15,13 +21,22 @@ export async function scroll(
   options: ScrollOptions
 ): Promise<void> {
   await emitScrollEvents(element, options.offset, options.steps);
+
+  if (options.momentumScroll) {
+    await emitMomentumEvents(
+      element,
+      options.offset,
+      options.momentumScroll.value,
+      options.momentumScroll.steps || 0
+    );
+  }
 }
 
 async function emitScrollEvents(
   element: ReactTestInstance,
   offset: ContentOffset,
   steps: number = 3
-) {
+): Promise<void> {
   let x = 0;
   let y = 0;
 
@@ -34,6 +49,52 @@ async function emitScrollEvents(
     })
   );
 
+  await emitIntermediateEvents(element, x, y, offset, steps);
+
+  dispatchEvent(element, 'scrollEndDrag', EventBuilder.Scroll.scroll(offset));
+}
+
+async function emitMomentumEvents(
+  element: ReactTestInstance,
+  offset: ContentOffset,
+  value: number,
+  steps: number
+): Promise<void> {
+  let x = offset.x || 0;
+  let y = offset.y || 0;
+
+  const momentumOffset: ContentOffset = {
+    x: offset.x ? value : 0,
+    y: offset.y ? value : 0,
+  };
+
+  const targetOffset: ContentOffset = {
+    x: offset.x ? offset.x + value : 0,
+    y: offset.y ? offset.y + value : 0,
+  };
+
+  dispatchEvent(
+    element,
+    'momentumScrollBegin',
+    EventBuilder.Scroll.scroll(offset)
+  );
+
+  await emitIntermediateEvents(element, x, y, momentumOffset, steps);
+
+  dispatchEvent(
+    element,
+    'momentumScrollEnd',
+    EventBuilder.Scroll.scroll(targetOffset)
+  );
+}
+
+async function emitIntermediateEvents(
+  element: ReactTestInstance,
+  x: number,
+  y: number,
+  offset: ContentOffset,
+  steps: number
+): Promise<void> {
   const offsetStepX = (offset.x || 0) / (steps + 1);
   const offsetStepY = (offset.y || 0) / (steps + 1);
 
@@ -49,6 +110,4 @@ async function emitScrollEvents(
       })
     );
   });
-
-  dispatchEvent(element, 'scrollEndDrag', EventBuilder.Scroll.scroll(offset));
 }
