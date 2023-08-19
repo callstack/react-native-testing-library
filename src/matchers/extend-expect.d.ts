33024c3fb3fdede94d2b4f08584847dd8680e6ae
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2023-08-20 00:16:17 +0200

    feat: add TS extends

---
+++ src/matchers/extend-expect.d.ts
@@ -0,0 +1,20 @@
+import { TextMatch, TextMatchOptions } from '../matches';
+
+export interface JestNativeMatchers<R> {
+  toBeOnTheScreen(): R;
+  toHaveTextContent(text: TextMatch, options?: TextMatchOptions): R;
+}
+
+// Implicit Jest global `expect`.
+declare global {
+  namespace jest {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface Matchers<R, T = {}> extends JestNativeMatchers<R> {}
+  }
+}
+
+// Explicit `@jest/globals` `expect` matchers.
+declare module '@jest/expect' {
+  interface Matchers<R extends void | Promise<void>>
+    extends JestNativeMatchers<R> {}
+}
