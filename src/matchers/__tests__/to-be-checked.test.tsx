bfa646b24b684e099db7f367dc504b9490c7f764
Kyaw Thura <kanaiyaaryal@gmail.com>
2023-08-31 23:03:17 +0700

    test: added more test

--- src/matchers/__tests__/to-be-checked.test.tsx
+++ src/matchers/__tests__/to-be-checked.test.tsx
@@ -1,19 +1,64 @@
 import React from 'react';
-import { View } from 'react-native';
+import { type AccessibilityRole, View } from 'react-native';
 import render from '../../render';
 import { screen } from '../../screen';
 import '../extend-expect';
 
-test('toBeCheck() with checkbox role', () => {
-  render(
-    <View
-      testID="view"
-      accessible
-      accessibilityRole="checkbox"
-      accessibilityState={{ checked: true }}
-    />
+const ViewWithRole = ({ role }: { role: AccessibilityRole }) => {
+  return (
+    <>
+      <View
+        testID={`${role}-checked`}
+        accessible
+        accessibilityRole={role}
+        accessibilityState={{ checked: true }}
+      />
+      <View
+        testID={`${role}-unchecked`}
+        accessible
+        accessibilityRole={role}
+        accessibilityState={{ checked: false }}
+      />
+      <View
+        testID={`${role}-mixChecked`}
+        accessible
+        accessibilityRole={role}
+        accessibilityState={{ checked: 'mixed' }}
+      />
+    </>
   );
+};
 
-  const view = screen.getByTestId('view');
-  expect(view).toBeChecked();
+test('toBeCheck() with checkbox role', () => {
+  render(<ViewWithRole role="checkbox" />);
+
+  const checkedView = screen.getByTestId('checkbox-checked');
+  const unCheckedView = screen.getByTestId('checkbox-unchecked');
+  const partiallyCheckedView = screen.getByTestId('checkbox-mixChecked');
+  expect(checkedView).toBeChecked();
+  expect(unCheckedView).not.toBeChecked();
+  expect(partiallyCheckedView).toBePartiallyChecked();
+
+  expect(() => expect(checkedView).not.toBeChecked()).toThrow();
+  expect(() => expect(unCheckedView).toBeChecked()).toThrow();
+  expect(() =>
+    expect(partiallyCheckedView).not.toBePartiallyChecked()
+  ).toThrow();
+});
+
+test('toBeCheck() with radio role', () => {
+  render(<ViewWithRole role="radio" />);
+
+  const checkedView = screen.getByTestId('radio-checked');
+  const unCheckedView = screen.getByTestId('radio-unchecked');
+  const partiallyCheckedView = screen.getByTestId('radio-mixChecked');
+  expect(checkedView).toBeChecked();
+  expect(unCheckedView).not.toBeChecked();
+  expect(partiallyCheckedView).toBePartiallyChecked();
+
+  expect(() => expect(checkedView).not.toBeChecked()).toThrow();
+  expect(() => expect(unCheckedView).toBeChecked()).toThrow();
+  expect(() =>
+    expect(partiallyCheckedView).not.toBePartiallyChecked()
+  ).toThrow();
 });
