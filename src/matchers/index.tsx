0bd5f15727f7be311170a67bf2d7fbbe93b66144
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2023-09-04 22:53:57 +0200

    chore: fix ts

--- src/matchers/index.tsx
+++ src/matchers/index.tsx
@@ -1,4 +1,9 @@
 export { toBeOnTheScreen } from './to-be-on-the-screen';
+export { toBeChecked } from './to-be-checked';
+export { toBeDisabled, toBeEnabled } from './to-be-disabled';
 export { toBeEmptyElement } from './to-be-empty-element';
+export { toBePartiallyChecked } from './to-be-partially-checked';
 export { toBeVisible } from './to-be-visible';
-export { toBeChecked, toBePartiallyChecked } from './to-be-checked';
+export { toHaveDisplayValue } from './to-have-display-value';
+export { toHaveProp } from './to-have-prop';
+export { toHaveTextContent } from './to-have-text-content';
