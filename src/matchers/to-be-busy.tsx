a61acb56e9217abe9cc53bde076348e6cee0b9f0
hduprat <hugod@bam.tech>
2023-09-07 13:30:29 +0200

    feat: implement toBeBusy() passing condition

--- src/matchers/to-be-busy.tsx
+++ src/matchers/to-be-busy.tsx
@@ -1,11 +1,15 @@
 import { ReactTestInstance } from 'react-test-renderer';
+import { matchAccessibilityState } from '../helpers/matchers/accessibilityState';
+import { checkHostElement } from './utils';
 
 export function toBeBusy(
   this: jest.MatcherContext,
   element: ReactTestInstance
 ) {
+  checkHostElement(element, toBeBusy, this);
+
   return {
-    pass: true,
+    pass: matchAccessibilityState(element, { busy: true }),
     message: () => '',
   };
 }
