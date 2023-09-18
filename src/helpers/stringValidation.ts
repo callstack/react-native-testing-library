70ec8ead54a25b9db35cfd617c1675bdb8e64c3a
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-09-18 13:10:17 +0200

    fix: update children handling

--- src/helpers/stringValidation.ts
+++ src/helpers/stringValidation.ts
@@ -16,12 +16,12 @@ export const validateStringsRenderedWithinText = (
 const validateStringsRenderedWithinTextForNode = (
   node: ReactTestRendererNode
 ) => {
-  if (typeof node === 'string') {
+  if (node == null || typeof node !== 'object') {
     return;
   }
 
   if (node.type !== 'Text') {
-    node.children?.forEach((child) => {
+    toChildrenArray(node.children).forEach((child) => {
       if (typeof child === 'string') {
         throw new Error(
           `Invariant Violation: Text strings must be rendered within a <Text> component. Detected attempt to render "${child}" string within a <${node.type}> component.`
@@ -31,6 +31,13 @@ const validateStringsRenderedWithinTextForNode = (
   }
 
   if (node.children) {
-    node.children.forEach(validateStringsRenderedWithinTextForNode);
+    toChildrenArray(node.children).forEach(
+      validateStringsRenderedWithinTextForNode
+    );
   }
 };
+function toChildrenArray(
+  children: ReactTestRendererNode | ReactTestRendererNode[]
+): ReactTestRendererNode[] {
+  return Array.isArray(children) ? children : [children];
+}
