49ad4e1ce76eda2defe3d2da5d90f99b6bc9ce5b
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-09-19 11:31:02 +0200

    refactors: cleanup

--- src/matchers/index.tsx
+++ src/matchers/index.tsx
@@ -1,13 +1,13 @@
-export { toBeOnTheScreen } from './to-be-on-the-screen';
+export { toBeBusy } from './to-be-busy';
 export { toBeChecked } from './to-be-checked';
 export { toBeDisabled, toBeEnabled } from './to-be-disabled';
-export { toBeBusy } from './to-be-busy';
 export { toBeEmptyElement } from './to-be-empty-element';
+export { toBeOnTheScreen } from './to-be-on-the-screen';
 export { toBePartiallyChecked } from './to-be-partially-checked';
+export { toBeSelected } from './to-be-selected';
 export { toBeVisible } from './to-be-visible';
+export { toContainElement } from './to-contain-element';
 export { toHaveDisplayValue } from './to-have-display-value';
 export { toHaveProp } from './to-have-prop';
 export { toHaveStyle } from './to-have-style';
 export { toHaveTextContent } from './to-have-text-content';
-export { toBeSelected } from './to-be-selected';
-export { toContainElement } from './to-contain-element';
