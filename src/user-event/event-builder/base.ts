dcbeaccb3c8eaf01050a430f289ace8bb26e5502
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-04-24 22:29:31 +0200

    refactor: self code review

--- src/user-event/event-builder/base.ts
+++ src/user-event/event-builder/base.ts
@@ -1,9 +1,15 @@
-export function baseSyntheticEvent() {
+import { BaseSyntheticEvent } from 'react';
+
+/** Builds base syntentic event stub, with prop values as inspected in RN runtime. */
+export function baseSyntheticEvent(): Partial<BaseSyntheticEvent<{}, unknown, unknown>> {
   return {
     currentTarget: {},
     target: {},
     preventDefault: () => {},
+    isDefaultPrevented: () => false,
     stopPropagation: () => {},
+    isPropagationStopped: () => false,
     persist: () => {},
+    timeStamp: 0,
   };
 }
