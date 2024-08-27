1eef2243e307ed9346452e398075e1d14a64c027
vanGalilea <galiliziv@gmail.com>
2024-08-27 19:57:20 +0200

    Debug tests failing in CI 3

--- examples/cookbook/app/network-requests/__tests__/PhoneBook.test.tsx
+++ examples/cookbook/app/network-requests/__tests__/PhoneBook.test.tsx
@@ -20,12 +20,10 @@ describe('PhoneBook', () => {
     screen.debug();
     await new Promise((resolve) => setTimeout(resolve, 1000));
     screen.debug();
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    screen.debug();
     // await waitForElementToBeRemoved(() => screen.getByText(/users data not quite there yet/i));
     expect(await screen.findByText('Name: Mrs Ida Kristensen')).toBeOnTheScreen();
-    // expect(await screen.findByText('Email: ida.kristensen@example.com')).toBeOnTheScreen();
-    // expect(await screen.findAllByText(/name/i)).toHaveLength(3);
+    expect(await screen.findByText('Email: ida.kristensen@example.com')).toBeOnTheScreen();
+    expect(await screen.findAllByText(/name/i)).toHaveLength(3);
   });
 
   // it('fails to fetch contacts and renders error message', async () => {
