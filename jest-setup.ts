b0d9a7e1c0212a643fad6b59b0e8ea7853878b4b
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-09-10 23:05:17 +0200

    chore: disable Jest mock

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,6 +1,6 @@
 import { resetToDefaults } from './src/pure';
 
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+//jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 beforeEach(() => {
   resetToDefaults();
