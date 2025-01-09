b869831054dbf8b82c0e14b61b7ece9827a542cd
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-01-09 10:30:17 +0100

    .

--- src/matchers/utils.ts
+++ src/matchers/utils.ts
@@ -1,4 +1,3 @@
-import type { ElementType } from 'react';
 import type { ReactTestInstance } from 'react-test-renderer';
 import {
   EXPECTED_COLOR,
@@ -8,10 +7,8 @@ import {
   RECEIVED_COLOR,
   stringify,
 } from 'jest-matcher-utils';
-import prettyFormat, { plugins } from 'pretty-format';
 import redent from 'redent';
 import { isHostElement } from '../helpers/component-tree';
-import { defaultMapProps } from '../helpers/map-props';
 
 class HostElementTypeError extends Error {
   constructor(received: unknown, matcherFn: jest.CustomMatcher, context: jest.MatcherContext) {
@@ -60,81 +57,6 @@ export type FormatElementOptions = {
   // Minimize used space.
   minimal?: boolean;
 };
-
-/***
- * Format given element as a pretty-printed string.
- *
- * @param element Element to format.
- */
-export function formatElement(
-  element: ReactTestInstance | null,
-  { minimal = false }: FormatElementOptions = {},
-) {
-  if (element == null) {
-    return '  null';
-  }
-
-  const { children, ...props } = element.props;
-  const childrenToDisplay = typeof children === 'string' ? [children] : undefined;
-
-  return redent(
-    prettyFormat(
-      {
-        // This prop is needed persuade the prettyFormat that the element is
-        // a ReactTestRendererJSON instance, so it is formatted as JSX.
-        $$typeof: Symbol.for('react.test.json'),
-        type: formatElementType(element.type),
-        props: defaultMapProps(props),
-        children: childrenToDisplay,
-      },
-      // See: https://www.npmjs.com/package/pretty-format#usage-with-options
-      {
-        plugins: [plugins.ReactTestComponent, plugins.ReactElement],
-        printFunctionName: false,
-        printBasicPrototype: false,
-        highlight: true,
-        min: minimal,
-      },
-    ),
-    2,
-  );
-}
-
-export function formatElementType(type: ElementType): string {
-  if (typeof type === 'function') {
-    return type.displayName ?? type.name;
-  }
-
-  // if (typeof type === 'object') {
-  //   console.log('OBJECT', type);
-  // }
-
-  if (typeof type === 'object' && 'type' in type) {
-    // @ts-expect-error: despite typing this can happen
-    const nestedType = formatElementType(type.type);
-    if (nestedType) {
-      return nestedType;
-    }
-  }
-
-  if (typeof type === 'object' && 'render' in type) {
-    // @ts-expect-error: despite typing this can happen
-    const nestedType = formatElementType(type.render);
-    if (nestedType) {
-      return nestedType;
-    }
-  }
-
-  return `${type}`;
-}
-
-export function formatElementArray(elements: ReactTestInstance[], options?: FormatElementOptions) {
-  if (elements.length === 0) {
-    return '  (no elements)';
-  }
-
-  return redent(elements.map((element) => formatElement(element, options)).join('\n'), 2);
-}
 
 export function formatMessage(
   matcher: string,
