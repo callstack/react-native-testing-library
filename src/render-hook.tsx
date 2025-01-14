560b7ca4c729028749d2e9ac48b105d516a09278
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-01-14 12:13:37 +0100

    .

--- src/render-hook.tsx
+++ src/render-hook.tsx
@@ -1,30 +1,42 @@
-import type { ComponentType } from 'react';
-import React from 'react';
+import * as React from 'react';
 import { renderInternal } from './render';
 
 export type RenderHookResult<Result, Props> = {
   rerender: (props: Props) => void;
-  result: { current: Result };
+  result: React.MutableRefObject<Result>;
   unmount: () => void;
 };
 
 export type RenderHookOptions<Props> = {
+  /**
+   * The initial props to pass to the hook.
+   */
   initialProps?: Props;
+
+  /**
+   * Pass a React Component as the wrapper option to have it rendered around the inner element. This is most useful for creating
+   * reusable custom render functions for common data providers.
+   */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  wrapper?: ComponentType<any>;
+  wrapper?: React.ComponentType<any>;
+
+  /**
+   * Set to `false` to disable concurrent rendering.
+   * Otherwise `renderHook` will default to concurrent rendering.
+   */
+  concurrentRoot?: boolean;
 };
 
 export function renderHook<Result, Props>(
-  renderCallback: (props: Props) => Result,
+  hookToRender: (props: Props) => Result,
   options?: RenderHookOptions<Props>,
 ): RenderHookResult<Result, Props> {
-  const initialProps = options?.initialProps;
-  const wrapper = options?.wrapper;
+  const { initialProps, ...renderOptions } = options ?? {};
 
   const result: React.MutableRefObject<Result | null> = React.createRef();
 
-  function TestComponent({ renderCallbackProps }: { renderCallbackProps: Props }) {
-    const renderResult = renderCallback(renderCallbackProps);
+  function TestComponent({ hookProps: hookProps }: { hookProps: Props }) {
+    const renderResult = hookToRender(hookProps);
 
     React.useEffect(() => {
       result.current = renderResult;
@@ -33,18 +45,20 @@ export function renderHook<Result, Props>(
     return null;
   }
 
-  const { rerender: baseRerender, unmount } = renderInternal(
+  const { rerender: componentRerender, unmount } = renderInternal(
     // @ts-expect-error since option can be undefined, initialProps can be undefined when it should'nt
-    <TestComponent renderCallbackProps={initialProps} />,
-    {
-      wrapper,
-    },
+    <TestComponent hookProps={initialProps} />,
+    renderOptions,
   );
 
-  function rerender(rerenderCallbackProps: Props) {
-    return baseRerender(<TestComponent renderCallbackProps={rerenderCallbackProps} />);
+  function rerender(hookProps: Props) {
+    return componentRerender(<TestComponent hookProps={hookProps} />);
   }
 
-  // @ts-expect-error result is ill typed because ref is initialized to null
-  return { result, rerender, unmount };
+  return {
+    // Result should already be set after the first render effects are run.
+    result: result as React.MutableRefObject<Result>,
+    rerender,
+    unmount,
+  };
 }
