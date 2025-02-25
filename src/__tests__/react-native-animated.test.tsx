35d63d0194cd0c18c72e2cdaee9fd7aebe3f2af6
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-02-25 17:18:57 +0100

    .

--- src/__tests__/react-native-animated.test.tsx
+++ src/__tests__/react-native-animated.test.tsx
@@ -43,7 +43,7 @@ describe('AnimatedView', () => {
     jest.useRealTimers();
   });
 
-  it('should use native driver when useNativeDriver is true', async () => {
+  it('should use native driver when useNativeDriver is true', () => {
     render(
       <AnimatedView fadeInDuration={250} useNativeDriver={true}>
         Test
@@ -56,7 +56,7 @@ describe('AnimatedView', () => {
     // expect(screen.root).toHaveStyle({ opacity: 0 });
   });
 
-  it('should not use native driver when useNativeDriver is false', async () => {
+  it('should not use native driver when useNativeDriver is false', () => {
     render(
       <AnimatedView fadeInDuration={250} useNativeDriver={false}>
         Test
