efe6e4acfa741e735b71ba7eaf18f09d62471c91
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-06-20 12:21:08 +0200

    chore: Jest 30 support

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,9 @@ module.exports = {
   setupFilesAfterEnv: ['./jest-setup.ts'],
   testPathIgnorePatterns: ['build/', 'examples/', 'experiments-app/', 'timer-utils'],
   testTimeout: 60000,
-  transformIgnorePatterns: ['/node_modules/(?!(@react-native|react-native)/).*/'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@react-native|react-native|react-native-gesture-handler)/).*/',
+  ],
   snapshotSerializers: ['@relmify/jest-serializer-strip-ansi/always'],
   clearMocks: true,
 };
