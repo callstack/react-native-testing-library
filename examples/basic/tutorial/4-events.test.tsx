4642563acbca4cf9bba82000346baa7594d7b835
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-07-10 16:30:50 +0200

    docs: examples

---
+++ examples/basic/tutorial/4-events.test.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { Text, View, Button } from 'react-native';
+import { fireEvent, render, screen, userEvent } from '@testing-library/react-native';
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <View>
+      <Text>{count}</Text>
+      <Button title="Increment" onPress={() => setCount(count + 1)} />
+    </View>
+  );
+}
+
+describe('Counter', () => {
+  it('should increment the count', async () => {
+    const user = userEvent.setup();
+
+    render(<Counter />);
+    expect(screen.getByText('0')).toBeOnTheScreen();
+
+    const button = screen.getByRole('button', { name: 'Increment' });
+    expect(button).toBeOnTheScreen();
+
+    await user.press(button);
+    expect(screen.getByText('1')).toBeOnTheScreen();
+  });
+
+  it('should increment the count when the button is pressed', async () => {
+    const user = userEvent.setup();
+
+    render(<Counter />);
+    expect(screen.getByText('0')).toBeOnTheScreen();
+
+    const button = screen.getByRole('button', { name: 'Increment' });
+    expect(button).toBeOnTheScreen();
+
+    await user.press(button);
+    expect(screen.getByText('1')).toBeOnTheScreen();
+  });
+});
