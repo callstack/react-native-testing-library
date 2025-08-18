45d93a8c1ec6ea404918253fc6bf33cbdca394c8
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-08-18 12:50:20 +0200

    .

--- src/helpers/__tests__/logger.test.ts
+++ src/helpers/__tests__/logger.test.ts
@@ -11,26 +11,38 @@ test('debug should call console.debug', () => {
   logger.debug('test message');
 
   expect(_console.debug).toHaveBeenCalledTimes(1);
-  expect(_console.debug).toHaveBeenCalledWith(expect.any(String));
+  expect(jest.mocked(_console.debug).mock.calls[0][0]).toMatchInlineSnapshot(`
+    "  ● test message
+    "
+  `);
 });
 
 test('should call console.info', () => {
   logger.info('info message');
 
   expect(_console.info).toHaveBeenCalledTimes(1);
-  expect(_console.info).toHaveBeenCalledWith(expect.any(String));
+  expect(jest.mocked(_console.info).mock.calls[0][0]).toMatchInlineSnapshot(`
+    "  ● info message
+    "
+  `);
 });
 
 test('should call console.warn', () => {
   logger.warn('warning message');
 
   expect(_console.warn).toHaveBeenCalledTimes(1);
-  expect(_console.warn).toHaveBeenCalledWith(expect.any(String));
+  expect(jest.mocked(_console.warn).mock.calls[0][0]).toMatchInlineSnapshot(`
+    "  ▲ warning message
+    "
+  `);
 });
 
 test('should call console.error', () => {
   logger.error('error message');
 
   expect(_console.error).toHaveBeenCalledTimes(1);
-  expect(_console.error).toHaveBeenCalledWith(expect.any(String));
+  expect(jest.mocked(_console.error).mock.calls[0][0]).toMatchInlineSnapshot(`
+    "  ■ error message
+    "
+  `);
 });
