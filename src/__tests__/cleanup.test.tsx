7c5f51f7a72d214de8e1ad52966feecb28e1f8a8
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-08-25 10:51:40 +0200

    more tests

--- src/__tests__/cleanup.test.tsx
+++ src/__tests__/cleanup.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { View } from 'react-native';
 
-import { cleanupAsync, render } from '../pure';
+import { cleanupAsync, render, renderAsync } from '../pure';
 
 class Test extends React.Component<{ onUnmount: () => void }> {
   componentWillUnmount() {
@@ -14,11 +14,22 @@ class Test extends React.Component<{ onUnmount: () => void }> {
   }
 }
 
-test('cleanup', async () => {
+test('cleanup after render', async () => {
   const fn = jest.fn();
 
   render(<Test onUnmount={fn} />);
   render(<Test onUnmount={fn} />);
+  expect(fn).not.toHaveBeenCalled();
+
+  await cleanupAsync();
+  expect(fn).toHaveBeenCalledTimes(2);
+});
+
+test('cleanup after renderAsync', async () => {
+  const fn = jest.fn();
+
+  await renderAsync(<Test onUnmount={fn} />);
+  await renderAsync(<Test onUnmount={fn} />);
   expect(fn).not.toHaveBeenCalled();
 
   await cleanupAsync();
