82933f482d8ae1faea81c00302780040d57b60e5
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-08-28 09:00:55 +0200

    chore: fix nightly test by excluding RCTSwitch style from snapshot

---
+++ src/test-utils/json.ts
@@ -0,0 +1,32 @@
+import type { ReactTestRendererJSON } from 'react-test-renderer';
+
+type JsonPropsMapper = {
+  [key: string]: unknown;
+};
+
+export function mapJsonProps<T extends ReactTestRendererJSON | ReactTestRendererJSON[] | null>(
+  element: T,
+  mapper: JsonPropsMapper,
+): T {
+  if (Array.isArray(element)) {
+    return element.map((e) => mapJsonProps(e, mapper)) as T;
+  }
+
+  if (!element) {
+    return element;
+  }
+
+  const resultProps = { ...element.props };
+  Object.keys(mapper).forEach((key) => {
+    if (key in element.props) {
+      resultProps[key] = mapper[key];
+    }
+  });
+
+  const resultElement = { ...element, props: resultProps };
+  Object.defineProperty(resultElement, '$$typeof', {
+    value: Symbol.for('react.test.json'),
+  });
+
+  return resultElement;
+}
