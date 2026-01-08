1c99145bc7b68ea6256abfdd6eb53babac5007de
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-01-09 00:07:43 +0100

    test(matchers/to-have-text-content): add negative (.not) variant test

--- src/matchers/__tests__/to-have-text-content.test.tsx
+++ src/matchers/__tests__/to-have-text-content.test.tsx
@@ -7,11 +7,16 @@ it('verifies element has expected text content', async () => {
   const Component = () => (
     <View>
       <Text testID="text-element">Hello World</Text>
+      <Text testID="other-element">Other Text</Text>
     </View>
   );
 
   await render(<Component />);
 
   const element = screen.getByTestId('text-element');
+  const otherElement = screen.getByTestId('other-element');
+
   expect(element).toHaveTextContent('Hello World');
+  expect(element).not.toHaveTextContent('Other Text');
+  expect(otherElement).not.toHaveTextContent('Hello World');
 });
