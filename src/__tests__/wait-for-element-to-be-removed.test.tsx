fb0d2171f275c176f4f3d6565d1d87a5646e5eb1
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-01-08 23:57:11 +0100

    test(wait-for-element-to-be-removed): validate resolution when query throws error after element removal

--- src/__tests__/wait-for-element-to-be-removed.test.tsx
+++ src/__tests__/wait-for-element-to-be-removed.test.tsx
@@ -1,7 +1,26 @@
-import { waitForElementToBeRemoved } from '..';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+import { render, screen, waitForElementToBeRemoved } from '..';
 
 it('throws error when element is already removed at the time of calling', async () => {
   await expect(waitForElementToBeRemoved(() => null)).rejects.toThrow(
     'The element(s) given to waitForElementToBeRemoved are already removed. waitForElementToBeRemoved requires that the element(s) exist(s) before waiting for removal.',
   );
+});
+
+it('resolves when query throws error after element is removed', async () => {
+  const Component = ({ showText }: { showText: boolean }) => (
+    <View>{showText && <Text>Hello</Text>}</View>
+  );
+
+  const { rerender } = await render(<Component showText={true} />);
+
+  expect(screen.getByText('Hello')).toBeOnTheScreen();
+
+  setTimeout(async () => {
+    await rerender(<Component showText={false} />);
+  }, 50);
+
+  await waitForElementToBeRemoved(() => screen.getByText('Hello'));
 });
