fe2d26f62338bfcbe997d0e71679404533a65a77
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-04-12 13:44:43 +0200

    chore: upgrade to react native 0.85

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'react-native',
+  preset: '@react-native/jest-preset',
   setupFilesAfterEnv: ['./jest-setup.ts'],
   testPathIgnorePatterns: ['build/', 'examples/', 'experiments-app/', 'codemods/'],
   testTimeout: 60000,
