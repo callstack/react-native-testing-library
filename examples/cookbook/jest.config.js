525ec6a2892b9bad8028d2e241f40ae8677eeeb0
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-06-03 11:30:55 +0200

    cookbook example

--- examples/cookbook/jest.config.js
+++ examples/cookbook/jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    '^react$': '<rootDir>/node_modules/react',
+    '^react/(.*)$': '<rootDir>/node_modules/react/$1',
+    '^react-native$': '<rootDir>/node_modules/react-native',
+    '^react-native/(.*)$': '<rootDir>/node_modules/react-native/$1',
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   setupFilesAfterEnv: ['./jest-setup.ts'],
   testMatch: ['**/*.test.{ts,tsx}'],
