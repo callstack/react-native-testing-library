15c4cfa217325fd096169b26b89c9b3ef611369b
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-06-03 11:33:13 +0200

    install jest presets

--- examples/basic/jest.config.js
+++ examples/basic/jest.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  preset: 'react-native',
-  moduleNameMapper: {
-    '^react$': '<rootDir>/node_modules/react',
-    '^react/(.*)$': '<rootDir>/node_modules/react/$1',
-    '^react-native$': '<rootDir>/node_modules/react-native',
-    '^react-native/(.*)$': '<rootDir>/node_modules/react-native/$1',
-  },
+  preset: '@react-native/jest-preset',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   transformIgnorePatterns: ['node_modules/(?!(jest-)?react-native|@react-native(-community)?)'],
   setupFilesAfterEnv: ['./jest-setup.ts'],
